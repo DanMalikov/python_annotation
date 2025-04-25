09944d037d90fe77c55b9089d9752995ba5cbf0e
Danila <danila.malikov.pd@gmail.com>
2025-04-25 21:50:45 +0300

    Добавление docstring в функции.

--- homework/inter_annotation.py
+++ homework/inter_annotation.py
@@ -4,7 +4,7 @@ from typing import Callable, Awaitable, ClassVar, Tuple, TypeVar, Literal, Liter
 
 def run_async(x: Awaitable[int]):
     """
-    Intermediate.async
+    Intermediate.await
     :return:
     """
     pass
@@ -14,72 +14,83 @@ SingleStringInput = Callable[[str], None]  # Intermediate.Callable
 
 
 class Fooo:
-    """Hint: No need to write __init__"""
+    """Intermediate.ClassVar"""
     bar: ClassVar[int]
 
 
 T = TypeVar("T", bound = Callable)
 def decorator(func: T) -> T:
+    """Intermediate.Decorator"""
     return func
 
 
 EmptyTuple = Tuple[()]
 
 def foo_empty_tuple(x: EmptyTuple):
+    """Intermediate.EmptyTuple"""
     pass
 
 
 Q = TypeVar("Q")
 
 def add_generic1(a: Q, b: Q) -> Q:
+    """Intermediate.Generic"""
     return a
 
 
 Z = TypeVar("Z", int, str)
 
 def add_generics2(a: Z, b: Z) -> Z:
+    """Intermediate.Generic2"""
     return a
 
 
 V = TypeVar("V", bound = int)
 
 def add_generics3(a: V) -> V:
+    """Intermediate.Generic3"""
     return a
 
 
 class Foo:
-    """Hint: you don't need to write __init__"""
+    """Intermediate.instance_var"""
     bar: int
 
 
 Side = Literal["left", "right"]
 
 def foo_literal(direction: Side):
+    """Intermediate.Literal"""
     ...
 
 
 def execute_query(sql: LiteralString, parameters: Iterable[str] = ...):
+    """Intermediate.Literalstring"""
     ...
 
 
 class FooSelf:
     def return_self(self):
+        """Intermediate.self"""
         return self
 
 
 class Student1(TypedDict):
+    """Intermediate.TypedDict"""
     name: str
     age: int
     school: str
 
 
 class Student2(TypedDict):
+    """Intermediate.TypedDict2"""
     name: str
     age: int
     school: NotRequired[str]
 
 
 class Person(TypedDict, total=False):
+    """Intermediate.TypedDict3"""
     name: Required[str]
     age: int
     gender: str
@@ -88,6 +99,7 @@ class Person(TypedDict, total=False):
 
 
 class Person2(TypedDict):
+    """Intermediate.Unpack"""
     name: str
     age: int
 
